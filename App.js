d442af441374c9e3e50d63fc6413b8efaa78e61c
Eckhard829 <eckharddednam@gmail.com>
2025-05-23 23:47:41 +0200

    added content

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ class App {
   constructor() {
     // Initial state
     this.user = JSON.parse(localStorage.getItem('user')) || null;
-    this.posts = JSON.parse(localStorage.getItem('posts')) || [
+    this.posts = [
       {
         id: '1',
         avatar: 'https://scontent-jnb2-1.cdninstagram.com/v/t51.2885-19/458315032_487378207491526_1272689892531119702_n.jpg?stp=dst-jpg_s100x100_tt6&_nc_cat=107&ccb=7-5&_nc_sid=bf7eb4&_nc_ohc=-bRrhh7pJREQ7kNvwE_8dDx&_nc_oc=AdlgM-nU-YTROt7Jnr9ET4QAggVS75YjHeF2gN3PY3EKW69j62AhVYoECPDNvtCLoLM&_nc_ad=z-m&_nc_cid=0&_nc_zt=24&_nc_ht=scontent-jnb2-1.cdninstagram.com&oh=00_AfJFvMPEM8cEickKHAf1pnuTw7xADpx5M-hlLqfhP8-7oA&oe=6833D622',
@@ -10,7 +10,8 @@ class App {
         time: '21min Ago',
         image: 'https://media.licdn.com/dms/image/v2/D4E03AQEw6JQ7ddN-Ww/profile-displayphoto-shrink_200_200/B4EZVGvv91H0Ak-/0/1740648677851?e=1753315200&v=beta&t=yS0YWYOEEhvU8cEyttOvY59BDhxHalnldh0riCBClEM',
         likes: '42',
-        caption: 'Success'
+        caption: 'Success',
+        comments: []
       },
       {
         id: '2',
@@ -19,16 +20,8 @@ class App {
         time: '3 Weeks Ago',
         image: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e',
         likes: '369',
-        caption: 'Remember the days when moments were lived, not posted? 💭'
-      },
-      {
-        id: '3',
-        avatar: 'https://scontent-jnb2-1.cdninstagram.com/v/t51.75761-19/499981008_18280277284248243_1846528086172006743_n.jpg?stp=dst-jpg_s150x150_tt6&_nc_cat=100&ccb=7-5&_nc_sid=f7ccc5&_nc_ohc=rY7eoUsnwoIQ7kNvwF4uFYr&_nc_oc=AdkJ9j1iiI_eoJpjhZ5tNhbAfAqozYf8aGRPex5yHhul8ptrwx51y4-O8BgfSYzCrxE&_nc_ad=z-m&_nc_cid=0&_nc_zt=24&_nc_ht=scontent-jnb2-1.cdninstagram.com&_nc_gid=E35GtQW7PW2jqkgalOOHhg&oh=00_AfKMwDrToNBcbvVrB8QeQj4QPHc9GKj6pQAJo_0zFPDs8g&oe=6833DCE5',
-        username: 'wtf_etas',
-        time: '2d Ago',
-        image: 'https://images.unsplash.com/photo-1519125323398-675f1f1d1f1f',
-        likes: '420',
-        caption: 'Yzerfontein mcnab attemp 1🫦'
+        caption: 'Remember the days when moments were lived, not posted? 💭',
+        comments: []
       }
     ];
     this.isLogin = true;
@@ -54,7 +47,6 @@ class App {
     this.$profileUsername = document.getElementById('profile-username');
     this.$profilePostCount = document.getElementById('profile-post-count');
     this.$profilePosts = document.getElementById('profile-posts');
-    this.$rightProfileImage = document.getElementById('right-profile-image');
     this.$rightProfileUsername = document.getElementById('right-profile-username');
     this.$logoutButton = document.getElementById('logout-button');
     this.$sidebarItems = document.querySelectorAll('.sidebar-item[data-section]');
@@ -136,6 +128,13 @@ class App {
     this.user = null;
     localStorage.removeItem('user');
     this.showAuth();
+    this.$authEmail.value = '';
+    this.$authPassword.value = '';
+    this.$authError.textContent = '';
+    window.scrollTo(0, 0);
+    this.isLogin = true;
+    this.$authSubmit.textContent = 'Log In';
+    this.$authToggle.textContent = "Don't have an account? Sign up";
   }
 
   handleCreatePost(e) {
@@ -157,10 +156,11 @@ class App {
         time: 'Just now',
         image: reader.result,
         likes: '0',
-        caption
+        caption,
+        comments: []
       };
       this.posts.unshift(newPost);
-      this.savePosts();
+      localStorage.setItem('posts', JSON.stringify(this.posts));
       this.$postImage.value = '';
       this.$postCaption.value = '';
       this.showSection('home');
@@ -181,7 +181,7 @@ class App {
 
   displayPosts() {
     this.$postSection.innerHTML = this.posts.map(post => `
-      <div class="post">
+      <div class="post" data-post-id="${post.id}">
         <div class="post-header">
           <div class="post-avatar" style="background-image: url(${post.avatar}); background-size: cover;"></div>
           <span class="post-username">${post.username}</span>
@@ -205,16 +205,67 @@ class App {
           <div class="post-caption">
             <span class="post-username">${post.username}</span> ${post.caption}
           </div>
-          <input type="text" placeholder="Add a comment..." class="post-comment-input">
+          <div class="post-comments" id="comments-${post.id}">
+            ${post.comments.map(comment => `
+              <div class="comment">
+                <span class="comment-username">${comment.username}</span>
+                <span class="comment-text">${comment.text}</span>
+              </div>
+            `).join('')}
+          </div>
+          <div class="post-comment-container">
+            <input type="text" placeholder="Add a comment..." class="post-comment-input" data-post-id="${post.id}">
+            <span class="material-symbols-outlined send-comment" data-post-id="${post.id}">send</span>
+          </div>
         </div>
       </div>
     `).join('');
+
+    // Add event listeners for comment submission
+    document.querySelectorAll('.send-comment').forEach(icon => {
+      icon.addEventListener('click', (e) => {
+        const postId = e.target.getAttribute('data-post-id');
+        this.handleAddComment(postId);
+      });
+    });
+
+    // Also allow pressing Enter to submit
+    document.querySelectorAll('.post-comment-input').forEach(input => {
+      input.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+          const postId = e.target.getAttribute('data-post-id');
+          this.handleAddComment(postId);
+        }
+      });
+    });
+  }
+
+  handleAddComment(postId) {
+    const commentInput = document.querySelector(`.post-comment-input[data-post-id="${postId}"]`);
+    const commentText = commentInput.value.trim();
+    
+    if (!commentText) return;
+    
+    if (!this.user) {
+      alert('Please log in to comment.');
+      return;
+    }
+
+    const post = this.posts.find(p => p.id === postId);
+    if (post) {
+      post.comments.push({
+        username: this.user.displayName,
+        text: commentText
+      });
+      localStorage.setItem('posts', JSON.stringify(this.posts));
+      this.displayPosts();
+      commentInput.value = '';
+    }
   }
 
   updateProfileUI() {
     this.$profileImage.src = this.user.photoURL;
     this.$profileUsername.textContent = this.user.displayName;
-    this.$rightProfileImage.src = this.user.photoURL;
     this.$rightProfileUsername.textContent = this.user.displayName;
     this.displayProfilePosts();
   }
@@ -225,10 +276,6 @@ class App {
     this.$profilePosts.innerHTML = userPosts.map(post => `
       <img src="${post.image}" alt="Post" class="profile-post-image">
     `).join('');
-  }
-
-  savePosts() {
-    localStorage.setItem('posts', JSON.stringify(this.posts));
   }
 }
 
